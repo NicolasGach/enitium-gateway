b5a3aceac1bcae6b3b3f84f61a4c23ee98fe1929
Nicolas Gach <nicolas.gach@worldline.com>
2021-12-26 10:58:15 +0100

    fix

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -9,6 +9,10 @@ import os
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
 w3 = Web3(Web3.HTTPProvider(os.environ['INFURA_NODE_URL']))
+CONTRACT_ADDRESS = os.environ['CONTRACT_ADDRESS']
+with open('EnitiumNFT.json', 'r') as f:
+    json_contract = json.load(f)
+CONTRACT_ABI = json_contract["abi"]
 
 def decrypt_sf_aes(content, key, vector):
     app.logger.info('vector : %s | content : %s', vector, content)
@@ -38,9 +42,10 @@ def sign_and_send_w3_transaction_transfer_type(w3, contract, builtTransaction, s
     return response
 
 global wait_and_process_receipt
-def wait_and_process_receipt(contract, tx_hash):
+def wait_and_process_receipt(tx_hash):
+    enitiumcontract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=CONTRACT_ABI)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash, poll_latency=0.5)
-    decoded_tx_receipt = contract.events.Transfer().processReceipt(tx_receipt)
+    decoded_tx_receipt = enitiumcontract.events.Transfer().processReceipt(tx_receipt)
     app.logger.info('tx_receipt : %s', tx_receipt)
     app.logger.info('tx_receipt decoded : %s', decoded_tx_receipt)
     response = {
