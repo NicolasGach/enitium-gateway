2f1cc3af0b6b2e46cdf4cff94bac07cd0fcdf606
GACH <nicolas.gach@worldline.com>
2022-04-27 19:22:09 +0200

    forgot to make the correction in some places ...

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -75,10 +75,10 @@ def process_mint(tx_uuid, tx, recipient_address, token_uri, bol_id):
         if db_highest_failed_nonce == pending_txs:
             tx['nonce'] = db_highest_failed_nonce
             tx['gas'] = tx['gas'] * FORCE_GAS_MULTIPLIER
-        if tx['gas'] > latest_block.gas_limit: tx['gas'] = latest_block.gasLimit - 1
+        if tx['gas'] > latest_block.gasLimit: tx['gas'] = latest_block.gasLimit - 1
     else:
         tx['gas'] = tx['gas'] * FORCE_GAS_MULTIPLIER
-        if tx['gas'] > latest_block.gas_limit: tx['gas'] = latest_block.gasLimit - 1
+        if tx['gas'] > latest_block.gasLimit: tx['gas'] = latest_block.gasLimit - 1
     enfty_tx = enitiumcontract.functions.mintNFT(recipient_address, token_uri).buildTransaction(tx)
     signed_transaction = w3.eth.account.sign_transaction(enfty_tx, OWNER_PRIVATE_KEY)
     try:
@@ -157,10 +157,10 @@ def process_transfer(tx_uuid, tx, from_address, from_pk, recipient_address, toke
         if db_highest_failed_nonce == pending_transactions:
             tx['nonce'] = db_highest_failed_nonce
             tx['gas'] = tx['gas'] * FORCE_GAS_MULTIPLIER
-            if tx['gas'] > latest_block.gas_limit: tx['gas'] = latest_block.gasLimit - 1
+            if tx['gas'] > latest_block.gasLimit: tx['gas'] = latest_block.gasLimit - 1
     else:
         tx['gas'] = tx['gas'] * FORCE_GAS_MULTIPLIER
-        if tx['gas'] > latest_block.gas_limit: tx['gas'] = latest_block.gasLimit - 1
+        if tx['gas'] > latest_block.gasLimit: tx['gas'] = latest_block.gasLimit - 1
     enfty_tx = enitiumcontract.functions.transferFrom(
         from_address,
         recipient_address,
